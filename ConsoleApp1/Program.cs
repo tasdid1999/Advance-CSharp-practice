7a80c72de92e3a43d2985113f1446cf5385b41a5
tasdid <tasdi@LAPTOP-D02SRM5Q>
2023-08-29 19:38:06 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Practice
+{
+
+    //practice delegates and Generic Class
+    public class Program
+    {
+       
+        public static void Main(String[] args)
+        {
+           CList<int> list = new CList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            var newList = list.Where(x => x % 2 == 0).ToList();
+
+            newList.Add(6);
+
+            foreach (var x in newList)
+            {
+                Console.WriteLine(x);
+            }
+                
+        }
+        
+
+    }
+    public class CList<T>
+    {
+        private List<T> List;
+
+        public CList()
+        {
+            List = new List<T>();
+        }
+
+        public List<T> Add(T data)
+        {
+            List.Add(data);
+            return List;
+        }
+            
+        
+        public CList<T> Where(Func<T ,bool> condition)
+        {
+            CList<T> list = new CList<T>();
+
+            //O(n) complexity
+            for(int i = 0; i < List.Count; i++)
+            {
+                if(condition(List[i]))
+                {
+                    list.Add(List[i]);
+                }
+            }
+
+            return list;
+        }
+        public List<T> ToList() => List;
+
+        
+       
+    }
+}
